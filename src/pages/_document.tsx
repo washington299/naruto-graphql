dd507d6ae318242ceb5a841cbc662719a1f2aed8
Washington Campos <washingtoncampos9@gmail.com>
2022-12-23 00:02:12 -0300

    feat: add naruto image on favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default function Document() {
 					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
 				/>
 				<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+				<link rel="icon" type="image/x-icon" href="/assets/images/naruto.png" />
 			</Head>
 			<body>
 				<Main />
