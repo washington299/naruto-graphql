d9861967767e1b4d3937985fbb9c26c60e3cec5b
Washington Campos <washingtoncampos9@gmail.com>
2022-12-23 02:57:34 -0300

    feat: create MenuMobile component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,106 +1,28 @@
-import { useState } from 'react';
 import NextLink from 'next/link';
-import {
-	AppBar,
-	Toolbar,
-	Typography,
-	Box,
-	Link,
-	IconButton,
-	Drawer,
-	List,
-	ListItem,
-	ListItemButton,
-	ListItemIcon,
-	ListItemText,
-	useMediaQuery,
-} from '@mui/material';
-import { Menu, Person, MapsHomeWork, Diversity3 } from '@mui/icons-material';
-import { useTheme } from '@mui/material/styles';
+import { AppBar, Toolbar, Typography, Box, Link } from '@mui/material';
 
 import { Logo } from '../Logo';
+import { MenuMobile } from '../MenuMobile';
 
-type MenuItemIconsProps = {
-	[key: string]: JSX.Element;
-};
-
-const menuItems = ['Characters', 'Villages', 'Clans'];
-const menuItemIcons: MenuItemIconsProps = {
-	Characters: <Person />,
-	Villages: <MapsHomeWork />,
-	Clans: <Diversity3 />,
-};
+export const menuItems = ['Characters', 'Villages', 'Clans'];
 
 export const Header = () => {
-	const [menuMobileOpen, setMenuMobileOpen] = useState(false);
-
-	const theme = useTheme();
-	const matches = useMediaQuery(theme.breakpoints.up('sm'));
-
-	const toggleMenuMobile = () => setMenuMobileOpen((previousState: boolean) => !previousState);
-
 	return (
 		<AppBar color="default">
 			<Toolbar sx={{ justifyContent: 'space-between' }}>
-				{!matches && (
-					<IconButton onClick={toggleMenuMobile}>
-						<Menu />
-					</IconButton>
-				)}
-				<Drawer
-					anchor="left"
-					open={menuMobileOpen}
-					onClose={toggleMenuMobile}
-					ModalProps={{ keepMounted: true }}
-				>
-					<Box width={300}>
-						<List>
-							{menuItems.map((item) => (
-								<Link
-									key={item}
-									component={NextLink}
-									href={`/${item.toLowerCase()}`}
-									underline="none"
-								>
-									<ListItem sx={{ padding: 0 }}>
-										<ListItemButton>
-											<ListItemIcon sx={{ minWidth: 40, height: 25 }}>
-												{menuItemIcons[item]}
-											</ListItemIcon>
-											<ListItemText sx={{ color: 'black', fontWeight: 500 }}>{item}</ListItemText>
-										</ListItemButton>
-									</ListItem>
-								</Link>
-							))}
-						</List>
-					</Box>
-				</Drawer>
-				<Box
-					sx={{
-						display: 'flex',
-						flex: 1,
-						justifyContent: { xs: 'center', sm: 'normal' },
-					}}
-				>
+				<MenuMobile />
+
+				<Box sx={{ display: 'flex', flex: 1, justifyContent: { xs: 'center', sm: 'normal' } }}>
 					<Logo />
 				</Box>
 
-				{matches && (
-					<Box component="nav" sx={{ display: 'flex', alignItems: 'center' }}>
-						{menuItems.map((item) => (
-							<Link
-								key={item}
-								component={NextLink}
-								href={`/${item.toLowerCase()}`}
-								underline="hover"
-								marginX={2}
-								color="black"
-							>
-								<Typography fontWeight={500}>{item}</Typography>
-							</Link>
-						))}
-					</Box>
-				)}
+				<Box component="nav" sx={{ display: { xs: 'none', sm: 'flex' }, alignItems: 'center' }}>
+					{menuItems.map((item) => (
+						<Link key={item} component={NextLink} href={`/${item.toLowerCase()}`} underline="hover">
+							<Typography sx={{ marginX: 2, color: 'black', fontWeight: 500 }}>{item}</Typography>
+						</Link>
+					))}
+				</Box>
 			</Toolbar>
 		</AppBar>
 	);
---
+++ src/components/MenuMobile/index.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import NextLink from 'next/link';
+import {
+	Box,
+	Link,
+	IconButton,
+	Drawer,
+	List,
+	ListItem,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText,
+} from '@mui/material';
+import { Menu, Person, MapsHomeWork, Diversity3 } from '@mui/icons-material';
+
+import { menuItems } from '../Header';
+
+type MenuItemIconsProps = {
+	[key: string]: JSX.Element;
+};
+
+const menuItemIcons: MenuItemIconsProps = {
+	Characters: <Person />,
+	Villages: <MapsHomeWork />,
+	Clans: <Diversity3 />,
+};
+
+export const MenuMobile = () => {
+	const [menuMobileOpen, setMenuMobileOpen] = useState(false);
+
+	const toggleMenuMobile = () => setMenuMobileOpen((previousState: boolean) => !previousState);
+
+	return (
+		<>
+			<IconButton sx={{ display: { xs: 'block', sm: 'none' } }} onClick={toggleMenuMobile}>
+				<Menu />
+			</IconButton>
+
+			<Drawer
+				anchor="left"
+				open={menuMobileOpen}
+				onClose={toggleMenuMobile}
+				ModalProps={{ keepMounted: true }}
+			>
+				<Box width={250}>
+					<List>
+						{menuItems.map((item) => (
+							<Link
+								key={item}
+								component={NextLink}
+								href={`/${item.toLowerCase()}`}
+								underline="none"
+							>
+								<ListItem sx={{ padding: 0 }}>
+									<ListItemButton>
+										<ListItemIcon sx={{ minWidth: 40, height: 25 }}>
+											{menuItemIcons[item]}
+										</ListItemIcon>
+										<ListItemText sx={{ color: 'black', fontWeight: 500 }}>{item}</ListItemText>
+									</ListItemButton>
+								</ListItem>
+							</Link>
+						))}
+					</List>
+				</Box>
+			</Drawer>
+		</>
+	);
+};
