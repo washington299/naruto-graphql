a35d4e6c8d27a109fc3128c0ab05c116708f0ce1
Washington Campos <washingtoncampos9@gmail.com>
2022-12-23 03:23:51 -0300

    refactor: move meta tag to _app.tsx file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { GlobalStyles } from '@mui/material';
 
 import { globalStyles } from 'styles/globalStyles';
@@ -6,6 +7,10 @@ import { globalStyles } from 'styles/globalStyles';
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<Head>
+				<meta name="viewport" content="initial-scale=1, width=device-width" />
+			</Head>
+
 			<GlobalStyles styles={globalStyles} />
 			<Component {...pageProps} />
 		</>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<meta name="viewport" content="initial-scale=1, width=device-width" />
 				<link
 					rel="stylesheet"
 					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
