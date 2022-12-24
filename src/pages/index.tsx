86389b750264242e20678ab7753e4f24c2d19b08
Washington Campos <washingtoncampos9@gmail.com>
2022-12-23 21:37:13 -0300

    feat: add title tag to Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
-import { Header } from '../components/Header';
+import Head from 'next/head';
+
+import { Header } from 'components/Header';
 
 export default function Home() {
-	return <Header />;
+	return (
+		<>
+			<Head>
+				<title>Naruto - Home</title>
+			</Head>
+			<Header />
+		</>
+	);
 }
