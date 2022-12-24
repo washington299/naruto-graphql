6dca5dddd1707430e9b0793e3e644d4332ab9b44
Washington Campos <washingtoncampos9@gmail.com>
2022-12-24 00:25:19 -0300

    chore: create HomeCards mock

---
+++ src/components/HomeCards/mock.ts
@@ -0,0 +1,20 @@
+export const homeCardsMock = [
+	{
+		src: '/assets/images/naruto-characters.png',
+		name: 'Characters',
+		description:
+			'Shinobi (忍), also interchangeably referred to as ninja (忍者), are the primary focus and the main military power in the series. A female ninja is known as a kunoichi (くノ一). Most come from a hidden village, of which some come from specialised ninja clans, and will perform missions for a fee. These missions can include: doing manual labour, acting as escorts, gathering secret information, retrieving stolen items, carrying out assassinations, etc. In times of war, ninja will come together to defend their villages and land. The clearest characteristic of a shinobi is their ability to manipulate chakra to create and use techniques. Shinobi are expected to be loyal to their villages for life, and any defectors are considered to be missing-nin, and will be marked for death.',
+	},
+	{
+		src: '/assets/images/naruto-villages.png',
+		name: 'Villages',
+		description:
+			'The Naruto series is set on a fictional terrestrial blue planet called Earth. Most of the series takes place on a large continent that is divided into a number of different countries; additional continents are occasionally depicted in supplementary media. This region and the society that prevails within it is often referred to as the Ninja World (忍界, Ninkai) or Shinobi World (忍者の世界, Shinobi no Sekai). ',
+	},
+	{
+		src: '/assets/images/naruto-clans.png',
+		name: 'Clans',
+		description:
+			'A clan (一族, Ichizoku, literally meaning: household, clan), in the loosest sense of the term, refers to a family or group of shinobi who form a basic unit of a shinobi village. A majority of these clans were mercenary military forces even before the First Shinobi World War.[1] The Senju clan and Uchiha clan were the first known clans to have agreed to settle and form a shinobi village, facilitated by creating agreements with the daimyō of the land they inhabit. Many clans would follow the example of the Senju and the Uchiha, later establishing the Five Great Shinobi Countries as they are known today. ',
+	},
+];
