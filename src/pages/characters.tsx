25256596dda1318979113f59492ab43acd9025b4
Washington Campos <washingtoncampos9@gmail.com>
2023-01-09 00:40:59 -0300

    feat: make graphql GET_CHARACTERS query

--- src/pages/characters.tsx
+++ src/pages/characters.tsx
@@ -1,10 +1,13 @@
 import Head from 'next/head';
+import { useQuery } from '@apollo/client';
 import { Typography, Grid } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Person from '@mui/icons-material/Person';
 import MapsHomeWork from '@mui/icons-material/MapsHomeWork';
 import Diversity3 from '@mui/icons-material/Diversity3';
+
+import { GET_CHARACTERS } from 'graphql/getCharacters';
 
 import { Container } from 'components/Container';
 import { CharacterCard } from 'components/CharacterCard';
@@ -16,6 +19,8 @@ type MenuItemIconsProps = {
 export default function Page() {
 	const theme = useTheme();
 	const matches = useMediaQuery(theme.breakpoints.up('lg'));
+
+	const { data, loading } = useQuery(GET_CHARACTERS);
 
 	const menuItemIcons: MenuItemIconsProps = {
 		characters: <Person fontSize="large" sx={{ marginLeft: 1 }} />,
@@ -38,53 +43,17 @@ export default function Page() {
 					Characters {menuItemIcons['characters']}
 				</Typography>
 
-				<Grid container spacing={matches ? 1 : 4}>
-					<Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-						<CharacterCard
-							id="any_id"
-							avatarSrc="https://narutoql.s3.amazonaws.com/Kimimaro.jpg"
-							name="(Kaguya) Kimimaro"
-							age={15}
-							rank="Unknown (Potential Jounin)"
-						/>
+				{loading ? (
+					<div>Loading...</div>
+				) : (
+					<Grid container spacing={matches ? 1 : 4}>
+						{data?.characters?.results?.map(({ id, avatarSrc, name, age, rank }) => (
+							<Grid key={id} item xs={12} sm={6} md={4} lg={3} xl={2.4}>
+								<CharacterCard id={id} avatarSrc={avatarSrc} name={name} age={age} rank={rank} />
+							</Grid>
+						))}
 					</Grid>
-					<Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-						<CharacterCard
-							id="any_id"
-							avatarSrc="https://narutoql.s3.amazonaws.com/YoroiMember.jpg"
-							name="Akadou Yoroi"
-							age={23}
-							rank="Genin"
-						/>
-					</Grid>
-					<Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-						<CharacterCard
-							id="any_id"
-							avatarSrc="https://narutoql.s3.amazonaws.com/Hoshikage.jpg"
-							name="Akahoshi"
-							age={null}
-							rank="Unknown (Probably Jounin)"
-						/>
-					</Grid>
-					<Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-						<CharacterCard
-							id="any_id"
-							avatarSrc="https://narutoql.s3.amazonaws.com/AkameIwana.jpg"
-							name="Akame Iwana"
-							age={32}
-							rank="Chuunin"
-						/>
-					</Grid>
-					<Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-						<CharacterCard
-							id="any_id"
-							avatarSrc="https://narutoql.s3.amazonaws.com/Choji.jpg"
-							name="Akimichi Chouji"
-							age={16}
-							rank="Chuunin - Team 10"
-						/>
-					</Grid>
-				</Grid>
+				)}
 			</Container>
 		</>
 	);
