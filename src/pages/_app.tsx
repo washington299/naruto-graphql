23d5de130b2d82459000c610169ec534679040e3
Washington Campos <washingtoncampos9@gmail.com>
2023-01-08 23:27:59 -0300

    chore: install and set up apollo graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { GlobalStyles } from '@mui/material';
 
 import { Layout } from 'components/Layout';
@@ -7,17 +8,24 @@ import { Layout } from 'components/Layout';
 import { globalStyles } from 'styles/globalStyles';
 
 export default function App({ Component, pageProps }: AppProps) {
+	const client = new ApolloClient({
+		uri: 'https://narutoql.up.railway.app/graphql',
+		cache: new InMemoryCache(),
+	});
+
 	return (
 		<>
 			<Head>
 				<meta name="viewport" content="initial-scale=1, width=device-width" />
 			</Head>
 
-			<GlobalStyles styles={globalStyles} />
+			<ApolloProvider client={client}>
+				<GlobalStyles styles={globalStyles} />
 
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</ApolloProvider>
 		</>
 	);
 }
