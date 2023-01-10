cd505b75f1b40bb9ca6554de61a3b32512f228bd
Washington Campos <washingtoncampos9@gmail.com>
2023-01-10 17:30:51 -0300

    feat: add pagination to characters page

--- src/pages/characters.tsx
+++ src/pages/characters.tsx
@@ -1,6 +1,7 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import { useQuery } from '@apollo/client';
-import { Typography, Grid, Box, CircularProgress } from '@mui/material';
+import { Typography, Grid, Box, CircularProgress, Pagination } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
@@ -13,7 +14,11 @@ export default function Page() {
 	const theme = useTheme();
 	const matches = useMediaQuery(theme.breakpoints.up('lg'));
 
-	const { data, loading } = useQuery(GET_CHARACTERS);
+	const [page, setPage] = useState(1);
+
+	const { data, loading } = useQuery(GET_CHARACTERS, { variables: { page } });
+
+	const handlePagination = (_e: React.ChangeEvent<unknown>, value: number) => setPage(value);
 
 	return (
 		<>
@@ -35,13 +40,25 @@ export default function Page() {
 						<CircularProgress />
 					</Box>
 				) : (
-					<Grid container spacing={matches ? 1 : 4}>
-						{data?.characters?.results?.map(({ id, avatarSrc, name, age, rank }) => (
-							<Grid key={id} item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-								<CharacterCard id={id} avatarSrc={avatarSrc} name={name} age={age} rank={rank} />
-							</Grid>
-						))}
-					</Grid>
+					<Box>
+						<Grid container spacing={matches ? 1 : 4}>
+							{data?.characters?.results?.map(({ id, avatarSrc, name, age, rank }) => (
+								<Grid key={id} item xs={12} sm={6} md={4} lg={3} xl={2.4}>
+									<CharacterCard id={id} avatarSrc={avatarSrc} name={name} age={age} rank={rank} />
+								</Grid>
+							))}
+						</Grid>
+
+						<Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 6 }}>
+							<Pagination
+								color="primary"
+								count={data?.characters?.info?.pages}
+								page={page}
+								siblingCount={0}
+								onChange={handlePagination}
+							/>
+						</Box>
+					</Box>
 				)}
 			</Container>
 		</>
