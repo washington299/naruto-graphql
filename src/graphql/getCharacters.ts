8336081acb0a3f597091a9f4146f845b87e05a9e
Washington Campos <washingtoncampos9@gmail.com>
2023-01-17 19:51:31 -0300

    chore: update query getCharacters types

--- src/graphql/getCharacters.ts
+++ src/graphql/getCharacters.ts
@@ -1,8 +1,8 @@
 import { gql } from 'graphql/__generated__/gql';
 
 export const GET_CHARACTERS = gql(`
-	query getCharacters($page: Int) {
-		characters(page: $page) {
+	query getCharacters($page: Int, $name: String) {
+		characters(page: $page, filter: { name: $name }) {
 			info {
 				count
 				pages
