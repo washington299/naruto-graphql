c6de5588fa02c99859474a00065195a706f2b63f
Washington Campos <washingtoncampos9@gmail.com>
2023-01-17 17:59:20 -0300

    feat: make SearchField component acessible

--- src/components/SearchField/index.tsx
+++ src/components/SearchField/index.tsx
@@ -1,4 +1,5 @@
-import { InputBase } from '@mui/material';
+import { useState, FormEvent } from 'react';
+import { InputBase, IconButton } from '@mui/material';
 import { styled, alpha } from '@mui/material/styles';
 import SearchIcon from '@mui/icons-material/Search';
 
@@ -6,9 +7,7 @@ const Search = styled('div')(({ theme }) => ({
 	display: 'flex',
 	alignItems: 'center',
 	maxWidth: 400,
-	width: '100%',
 	paddingLeft: theme.spacing(2),
-	paddingRight: theme.spacing(1),
 	marginBottom: 24,
 	border: `1px solid ${theme.palette.grey[300]}`,
 	borderRadius: theme.shape.borderRadius,
@@ -21,7 +20,6 @@ const Search = styled('div')(({ theme }) => ({
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
 	flex: 1,
 	width: '100%',
-	color: 'inherit',
 	'& .MuiInputBase-input': {
 		padding: theme.spacing(1, 1, 1, 0),
 		transition: theme.transitions.create('width'),
@@ -33,22 +31,32 @@ type SearchFieldProps = {
 };
 
 export const SearchField = ({ disabled = false }: SearchFieldProps) => {
+	const [value, setvalue] = useState('');
+
+	const isInputEmpty = value.length === 0;
+
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		if (isInputEmpty) return;
+
+		console.log(value);
+	};
+
 	return (
-		<Search>
-			<StyledInputBase
-				placeholder="Search…"
-				inputProps={{ 'aria-label': 'search' }}
-				disabled={disabled}
-			/>
-			<SearchIcon
-				style={{
-					marginTop: 1,
-					fontSize: 28,
-					cursor: 'pointer',
-					pointerEvents: disabled ? 'none' : 'all',
-				}}
-				onClick={() => console.log('success')}
-			/>
-		</Search>
+		<form onSubmit={handleSubmit}>
+			<Search>
+				<StyledInputBase
+					placeholder="Search character..."
+					inputProps={{ 'aria-label': 'search' }}
+					disabled={disabled}
+					value={value}
+					onChange={(e) => setvalue(e.target.value)}
+				/>
+				<IconButton type="submit" sx={{ pointerEvents: disabled || isInputEmpty ? 'none' : 'all' }}>
+					<SearchIcon style={{ marginTop: 1, fontSize: 28, cursor: 'pointer' }} />
+				</IconButton>
+			</Search>
+		</form>
 	);
 };
