39efa9c5ce8312a7365ba7f6bc32ee0db881269c
Washington Campos <washingtoncampos9@gmail.com>
2023-01-21 00:04:21 -0300

    feat: add villages data to villages page

--- src/pages/villages.tsx
+++ src/pages/villages.tsx
@@ -1,10 +1,17 @@
 import Head from 'next/head';
 import { Typography, Grid } from '@mui/material';
 
+import { useGetVillages } from 'graphql/getVillages';
+
 import { Container } from 'components/Container';
 import { Card } from 'components/Card';
+import { Loading } from 'components/Loading';
 
 export default function Villages() {
+	const { loading, data } = useGetVillages();
+
+	console.log(data);
+
 	return (
 		<>
 			<Head>
@@ -16,15 +23,21 @@ export default function Villages() {
 					Villages
 				</Typography>
 
-				<Grid container spacing={4}>
-					<Grid item xs={12} md={6} lg={4}>
-						<Card
-							src="/assets/images/cloud-village.png"
-							name="Cloud Village"
-							description="Mollit nostrud laboris nisi tempor consequat laborum aliquip consequat labore nisi ullamco. Esse laborum do labore proident adipisicing laborum ut adipisicing occaecat consequat velit. Esse labore eiusmod consequat nulla aliquip. Esse aliqua minim magna fugiat culpa."
-						/>
+				{loading ? (
+					<Loading />
+				) : (
+					<Grid container spacing={4}>
+						{data?.villages?.results?.map(({ _id, name }) => (
+							<Grid key={_id} item xs={12} md={6} lg={4}>
+								<Card
+									src="/assets/images/cloud-village.png"
+									name={name}
+									description="Mollit nostrud laboris nisi tempor consequat laborum aliquip consequat labore nisi ullamco. Esse laborum do labore proident adipisicing laborum ut adipisicing occaecat consequat velit. Esse labore eiusmod consequat nulla aliquip. Esse aliqua minim magna fugiat culpa."
+								/>
+							</Grid>
+						))}
 					</Grid>
-				</Grid>
+				)}
 			</Container>
 		</>
 	);
