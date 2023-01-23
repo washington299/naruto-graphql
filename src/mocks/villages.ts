f90acbbc992275db3157b3ecc8275525fcfd1d45
Washington Campos <washingtoncampos9@gmail.com>
2023-01-23 16:59:43 -0300

    feat: create villages mock

---
+++ src/mocks/villages.ts
@@ -0,0 +1,94 @@
+type VillagesMockTypes = {
+	[key: string]: {
+		img: string;
+		description: string;
+	};
+};
+
+export const villagesMock: VillagesMockTypes = {
+	'61bd1dbc918f12c17b9c6460': {
+		img: '/assets/images/cloud-village.png',
+		description: `
+			Kumogakure (雲隠れの里, Kumogakure no Sato, English TV: Hidden Cloud Village, literally meaning: Village Hidden by Clouds) is the hidden village of the Land of Lightning and was founded by the First Raikage. As the village of one of the Five Great Shinobi Countries, Kumogakure has a Kage as its leader known as the Raikage, of which there have been five in its history. The village is located in a range of tall mountains, and is literally hidden in the clouds. The Raikage works in a large blue structure built into the tallest mountain.
+			The standard attire for Kumo shinobi consists of a long, grey top which gathers just at the waist to give a sash-like appearance, with a matching coloured bottom. Over this they wear white, one-strapped flak jackets and arm as well as shin-guards. Many Kumogakure ninja use Lightning Release techniques. `,
+	},
+	'61bd1dbc918f12c17b9c646b': {
+		img: '/assets/images/craftsman-village.png',
+		description: `Takumi Village (匠の里, Takumi no Sato, literally meaning: Village of Artisans) is an anime-only village that resides in the Land of Rivers. It was founded approximately one hundred years ago by a man named Seimei. Takumi is known for its skills in making weaponry, many of which are sold to the ninja villages of the world. However, each time relations worsened between the larger villages, Takumi was attacked by one village over fear of their ninja tools crossing to the other village. Yet they still had to curry aid with the larger villages to survive. Over time, their clientele from the other villages grew in skill, making the crafted weapons obsolete. When the village faced hard times they requested help but were ignored. Certain skilled Takumi villagers grew resentful and felt the villages didn't respect them for all the help their weapons provided in the past, having repaid their aid with attacks in the past. `,
+	},
+	'61bd1dbc918f12c17b9c6464': {
+		img: '/assets/images/grass-village.png',
+		description: `Kusagakure (草隠れの里, Kusagakure no Sato, literally meaning: Village Hidden in the Grass) is located thus far in an unnamed country. Because it had genin participating in the Chūnin Exams organised by Konohagakure, it can be assumed the two villages are allied. Although it has never been named, the country Kusagakure is located in has been shown on several occasions. The country is rich with forests, some consisting of giant mushrooms or bamboo. It also seems to have many rivers and deep ravines.
+			During the Third Shinobi World War, the country was invaded by Iwagakure and served as an important front between Iwa and Konoha. More than a decade later, Orochimaru had a hideout somewhere in the country.
+			Kusagakure is skilled in diplomacy, a trait they constantly use to read the movements of other countries and stay one step ahead of them. They also analyse techniques from other villages in a similar manner. Because of these principles, others find it hard to read the shinobi from this village's attitudes.
+		`,
+	},
+	'61bd1dbc918f12c17b9c646a': {
+		img: '/assets/images/heat-haze-village.png',
+		description: `Kagerō Village (陽炎の里, Kagerō no Sato, English TV: Heat Devil Village, literally meaning: Heat Haze Village), was a small village located in the Land of Mountains. Thirty years before the start of the series, it was at war with Konoha. Three months after a surprise attack by the Third Hokage, Jiraiya and several other Konoha ninja, they declared a cease-fire. Two weeks later, however, they were wiped out by shinobi from Tanigakure. Only Gennō, who had been in Konoha at the time planting explosive tags for a possible counter-attack, survived its destruction. `,
+	},
+	'61bd1dbc918f12c17b9c6466': {
+		img: '/assets/images/hot-springs-village.png',
+		description: `Yugakure (湯隠れの里, Yugakure no Sato, literally meaning: Village Hidden in Hot Water) is the hidden village of the Land of Hot Water. Like the country in which it resides, Yugakure has strong inclinations towards pacifism. After years of reluctantly offering combat-related services, Yugakure officially transitioned away from the typical shinobi missions; it prides itself as the "village that has forgotten wars" (戦を忘れた里, Ikusa o Wasureta Sato). The village's ninja now work almost exclusively within the Land of Hot Water's borders, keeping the roads safe for tourists and performing odd-jobs for the many small settlements around the country.
+			Yugakure's transformation from a military force was not supported by all, Hidan being a notable objector. A follower of Jashin, Hidan believed violence was good, even necessary; when he couldn't convince the village to change its ways, he became a missing-nin, slaughtering many of his fellow Yu-nin before he left.
+		`,
+	},
+	'61bd1dbc918f12c17b9c645d': {
+		img: '/assets/images/leaf-village.png',
+		description: `Konohagakure (木ノ葉隠れの里, Konohagakure no Sato, English TV: "Village Hidden in the Leaves" or "Hidden Leaf Village", literally meaning: Village Hidden by Tree Leaves) is the hidden village of the Land of Fire. As the village of one of the Five Great Shinobi Countries, Konohagakure has a Kage as its leader known as the Hokage, of which there have been seven in its history. Konoha resides deep within a forest at the base of a mountain known as the Hokage Rock, which has the faces of all those who have taken the office of Hokage engraved on it.[1] It is surrounded on all sides by enormous walls.[2] While generally seen as the most powerful of the ninja villages,[3] Konoha has enjoyed many years of relative peace and stability.
+			The standard attire for Konoha shinobi consists of blue or black shirts which may or may not have swirl patterns on the shoulders, along with matching coloured pants under a green flak jacket which also has a red swirl on the back, and pockets on the chest area. They also tend to wrap bandages around their legs. Many Konohagakure ninja use Fire Release techniques.
+		`,
+	},
+	'61bd1dbc918f12c17b9c646c': {
+		img: '/assets/images/lock-village.png',
+		description: `The Jōmae Village (錠前の里, Jōmae no Sato, literally meaning: Lock Village) is an anime-only shinobi village located in the Land of Keys. This village's shinobi are experts in espionage in order to gain information for negotiations with larger, more powerful nations. In this village, ninja seem to be specifically trained in gaining information while giving none in return as evident by Hanare's Eye Mind Reading technique and their leader's willingness to commit suicide rather than be taken hostage by enemy ninja.`,
+	},
+	'61bd1dbc918f12c17b9c645f': {
+		img: '/assets/images/mist-village.png',
+		description: `Kirigakure (霧隠れの里, Kirigakure no Sato, English TV: Village Hidden in the Mist, literally meaning: Village Hidden by Mist) is the hidden village of the Land of Water. As the village of one of the Five Great Shinobi Countries, Kiri has a Kage as its leader known as the Mizukage, of which there have been six in its history. Befitting its name, the village is surrounded by a thick mist, with several mountains in the background, making it difficult to find by foreigners. Kirigakure's architecture is composed of several cylindrical buildings, with the Mizukage's office being the widest and largest. Most of the buildings have vegetation growing on their roofs.
+			The standard attire for Kiri shinobi revolves around a pinstriped material which the shinobi wear as bracers and greaves which merge into their sandals. Underneath this, they wear a full-black shirt and pants over which they wear the villages' standard grey flak jacket. Some also wear waist-guards. Many Kiri ninja use Water Release techniques. Due to the village's high number of missing-nin, Kiri has a subdivision of Anbu known as hunter-nin that are specifically tasked with eliminating these defectors.
+		`,
+	},
+	'61bd1dbc918f12c17b9c646d': {
+		img: '/assets/images/pink-flower-village.png',
+		description: `Nadeshiko Village (Nadeshiko no Sato, liternal meaning for "Ninje Village of Pink Flowers" or "Pink Flower Village") is a ninja village located near the Land of Water. The families of this village are organized through matrilineality, a women-populace village. Women from this village must venture outside to find men strong enough to defeat them, resulting in them being taken to the village to marry the kunoichi they defeated.
+			It is unknown if this village had taken part in the last Three Great Ninja Wars, but after the Second Great Ninja War, Jiraiya visited Nadeshiko as he was "researching" for his next book, the Nadeshiko kunochi spotted him and try to stop him, the future leader, Rikuka was the only one to chanllenge Jiraiya in a duel to have his hand in marriage.
+		`,
+	},
+	'61bd1dbc918f12c17b9c6462': {
+		img: '/assets/images/rain-village.png',
+		description: `Amegakure (雨隠れの里, Amegakure no Sato, literally meaning: Village Hidden by Rain) is a small but highly industrialised hidden village located in an unnamed country. As its name suggests, it rains almost constantly in Amegakure and the surrounding area. All the water seemingly ends in a large lake that surrounds the village.
+			Ame ninja are said to be notoriously short tempered, while their village trains them to their fullest potential, resulting in the creation of many original techniques. Ame is frequently hired for assassination missions, an area of expertise prized by many countries. A common trait of shinobi from this village is wearing rebreathers. Its shinobi also have a propensity for genjutsu.
+		`,
+	},
+	'61bd1dbc918f12c17b9c645e': {
+		img: '/assets/images/rock-village.png',
+		description: `Iwagakure (岩隠れの里, Iwagakure no Sato, English TV: Hidden Stone Village, literally meaning: Village Hidden by Rocks) is the hidden village of the Land of Earth. As the village of one of the Five Great Shinobi Countries, Iwagakure has a Kage as its leader known as the Tsuchikage, of which there have been four in its history. The rocky mountain ranges that surround the village provide a natural stronghold that it is very proud of. The village's infrastructure is built from much of the surrounding rock and stone, shaped into tower-like structures that are inter-connected by a network of bridges. The Tsuchikage's residence seems to be the tallest structure with a cone-shaped roof and bears the kanji for "Earth" (土, Tsuchi) on it.
+			The standard attire for Iwa shinobi consists of red outfit — which can either have one, or both sleeves — a lapel which is usually found on the side without a sleeve and a brown flak jacket — which also may, or may not have a pouch attached to it. They also tend to wear mesh armour around their ankles. Many Iwagakure ninja use Earth Release techniques. Iwagakure is well-known for the rock-hard attitude of its shinobi; as soon as the Tsuchikage gives an order, the Iwa-nin follow it without hesitation, even if it means death
+		`,
+	},
+	'61bd1dbc918f12c17b9c6461': {
+		img: '/assets/images/sand-village.png',
+		description: `Sunagakure (砂隠れの里, Sunagakure no Sato, literally meaning: Village Hidden by Sand) is the hidden village of the Land of Wind. As the village of one of the Five Great Shinobi Countries, Sunagakure has a Kage as its leader known as the Kazekage, of which there have been five in its history. Being surrounded by desert offers the village a natural deterrent against invasion, as few foreign powers would be willing to endure the sandstorms and water scarcity common to Suna. Sunagakure itself lies in a fortified valley behind cliffs of rock, with passage in and out of the village restricted to a single cleft between two cliff faces, making the area very hard to attack from the ground. The buildings inside seem to be made of clay or stucco, which would help to keep the buildings cool.`,
+	},
+	'61bd1dbc918f12c17b9c6465': {
+		img: '/assets/images/sound-village.png',
+		description: `Otogakure (音隠れの里, Otogakure no Sato, English TV: Village Hidden in the Sound, literally meaning: Village Hidden by Sound) is the personal hidden village of Orochimaru, which was founded for the express purpose of collecting ninja for his experiments and his quest to learn all techniques. In later years, following Orochimaru's new appreciation for natural development of people and making a truce with Konohagakure, Otogakure was reestablished as a legitimate shinobi village.`,
+	},
+	'61bd1dbc918f12c17b9c6469': {
+		img: '/assets/images/star-village.png',
+		description: `Hoshigakure (星隠れの里, Hoshigakure no Sato, literally meaning: Village Hidden Among Stars) was introduced during the Star Guard Mission. It is founded by the First Hoshikage two hundred years before the start of the series and is located in the Land of Bears. It is also one of the lesser-known shinobi villages in the Naruto universe.`,
+	},
+	'61bd1dbc918f12c17b9c6463': {
+		img: '/assets/images/waterfall-village.png',
+		description: `Takigakure (滝隠れの里, Takigakure no Sato, literally meaning: Village Hidden by a Waterfall) is a village located in an unnamed country. In terms of the village's appearance, Takigakure seems to be based on mostly Southeast Asian aesthetics. Their current leader is Shibuki.
+		Around the time of the village's formation, the village tasked Kakuzu with assassinating Hashirama Senju, the First Hokage. Facing the might of the Wood Release, Kakuzu returned in failure. Despite the insurmountable danger faced on this mission, what awaited his return to his village was the stigma and harsh punishment associated with the failure of his objective. In response to this treatment, a wrong levied against a shinobi who had put life and limb on the line for his village, Kakuzu came to hold a violent hatred for Takigakure as he escaped from imprisonment. In his flight from the village, Kakuzu killed the village elders, took their hearts, and fled with knowledge of the village's most prized forbidden techniques.
+		`,
+	},
+	'61bd1dbc918f12c17b9c6467': {
+		img: '/assets/images/whirling-tides-village.png',
+		description: `Uzushiogakure (渦潮隠れの里, Uzushiogakure no Sato, English TV: Hidden Eddy Village, literally meaning: Village Hidden by Whirling Tides) was the shinobi village of the Land of Whirlpools. Its ninja were renowned for their fūinjutsu to the point that it led to its destruction in war. Those who survived the village's destruction scattered across the globe to seek refuge.
+			The people of Uzushiogakure were noted to have notoriously long lives, so it gained the epithet "The Village of Longevity" (長寿の里, Chōju no Sato). In terms of appearance, the village seemed to have been composed of several high-rise buildings. A wide river seemingly ran through the village and was gapped by large bridges. The surrounding countryside was dominated by steep hills.
+		`,
+	},
+};
