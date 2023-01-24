4280ad82797fe5607ff1ae869a79d8c054961fba
Washington Campos <washingtoncampos9@gmail.com>
2023-01-24 15:57:23 -0300

    feat: create clansMock data

---
+++ src/mocks/clans.ts
@@ -0,0 +1,134 @@
+type ClansMockTypes = {
+	[key: string]: {
+		img: string;
+		description: string;
+	};
+};
+
+export const clansMock: ClansMockTypes = {
+	'61bd1dbc918f12c17b9c646e': {
+		img: '/assets/images/aburame-clan.png',
+		description: `The Aburame Clan (油女一族, Aburame Ichizoku) is one of the four noble clans of Konohagakure.[1] At birth, members of this clan are offered to several special breeds of insects as a nest, residing just under their host's skin. These insects will then live in symbiosis with their host from that point on. Because of this, its members are characterised by their use of insects as weapons.
+			The clan members may also carry additional insects in jars or gourds with them on missions. The insects can leave and enter their host's body through various pores. They feed on chakra as a food source, making them quite deadly. The relationship between the shinobi and the insects is mutually beneficial.
+		`,
+	},
+	'61bd1dbc918f12c17b9c646f': {
+		img: '/assets/images/akimichi-clan.png',
+		description: `The Akimichi Clan (秋道一族, Akimichi Ichizoku) is one of the four noble clans of Konohagakure.[1] Many of their clan's techniques revolve around the manipulation of their bodyweight and size through the use of Yang Release. It has had fifteen family heads, with Chōza Akimichi being the fifteenth. Chōji Akimichi is slated to become the sixteenth head.
+			Members of the clan possess great physical strength and are able to quickly convert calories into chakra, which they then use in their various secret techniques. Most of these techniques rapidly consume the user's chakra during use, and maintaining them during a prolonged battle can be tiring. For this reason the Akimichi have high chakra levels and eat a lot in order to build up or replenish their chakra reserves.
+		`,
+	},
+	'61bd1dbc918f12c17b9c6470': {
+		img: '/assets/images/fumma-clan.png',
+		description: `The Fūma Clan (ふうま一族, Fūma Ichizoku) is a shinobi clan from the Land of the Rice Fields, where Orochimaru established the Land of Sound. The clan specialises in using chakra threads and also have a large number of shinobi trained in archery. It also has a number of hidden techniques, including the Curse Mandala. The clan split into two factions where some of their most powerful members were tricked by Orochimaru, who promised to restore their clan's status, and were used in his kinjutsu development experiments. The other faction became involved in criminal activity and were considered traitors by the rest of the clan.`,
+	},
+	'61bd1dbc918f12c17b9c6471': {
+		img: '/assets/images/houzuki-clan.png',
+		description: `The Hozuki Clan is a ninja that originated from Kirigakure.
+			The clan possess the ability to turn their bodies into a liquid form using the Hydration Jutsu. This ability however seems to require that they stay hydrated at all time, so they carry water bottles around with them. They also specialize in Ice Style ninjutsu and taijutsu and the one known as "Ice Fist". Since the clan follows the ways of the Ice Dragons, they are basically Ice Dragon Wielders.
+			The Hozuki were one of the clans responsible in the founding of Kirigakure, along with the Gyokana Clan and befriended the Senju Clan, Terumi Clan, and Yuki Clan.
+		`,
+	},
+	'61bd1dbc918f12c17b9c6472': {
+		img: '/assets/images/hyuuga-clan.png',
+		description: `The Hyūga Clan (日向一族, Hyūga Ichizoku) is one of the four noble clans of Konohagakure, as well as the strongest clan in the village. All members born into this clan possess the Byakugan, a kekkei genkai that gives them extended fields of vision and the ability to see through solid objects and even the chakra circulatory system, amongst other things. Members of this clan also possess an innate ability which allows them to expel chakra from any of the tenketsu in their body.
+			The Hyūga clan are descendants from the Ōtsutsuki clan, specifically from Hamura Ōtsutsuki's lineage. As a result, they are also distant cousins of the Uchiha, Senju, Uzumaki, and Kaguya clans. They are considered to be one of, if not currently, the most powerful clan in Konohagakure. Their fame and prowess stretches far and wide, leading others to both praise and covet their abilities. During the Fourth Shinobi World War, a Kumo-nin noted that they had a hard time against them during a previous war.
+		`,
+	},
+	'61bd1dbc918f12c17b9c6473': {
+		img: '/assets/images/inuzuka-clan.png',
+		description: `The Inuzuka Clan (犬塚一族, Inuzuka Ichizoku) or Inuzuka Family (犬塚家, Inuzuka-ke) is a family of shinobi in Konohagakure known for their use of ninken as fighting companions and are easily identified by the distinctive red fang markings on their cheeks.
+			The members are given their own canine partner(s) when they reach a certain age. Thereafter, the shinobi and their dog(s) are practically inseparable. The shinobi and canine allies fight using Cooperation Ninjutsu, such as the Fang Passing Fang or Fang Rotating Fang techniques amongst others, that take advantage of their teamwork and their sharp claws and teeth.
+		`,
+	},
+	'61bd1dbc918f12c17b9c6474': {
+		img: '/assets/images/kaguya-clan.png',
+		description: `The Kaguya Clan (かぐや一族, Kaguya Ichizoku) is a now extinct clan, known for their savage battle tactics and archaic values, and as descendants of Kaguya Ōtsutsuki, a select few have inherited a derivative of her original ability to manipulate one's bone structures.
+			The Kaguya clan were descendants of Kaguya Ōtsutsuki, and selective few, namely Kimimaro, possessed the kekkei genkai Shikotsumyaku from Kaguya's original bone-manipulating ability. This ability was so powerful that even the war-loving Kaguya clan feared it.
+		`,
+	},
+	'61bd1dbc918f12c17b9c6475': {
+		img: '/assets/images/kamizuru-clan.png',
+		description: `The Kamizuru Clan (上水流一族, Kamizuru Ichizoku) was a clan of bee users from Iwagakure who were once very well-known and revered. Suzumebachi claims that the First Tsuchikage was a member of the clan.
+			Years before the start of the series, they led an attempted invasion by Iwa of Konohagakure, only to be repelled and largely killed by Konoha's more experienced Aburame clan. The Kamizuru were so badly defeated that they lost their status and respect in Iwa and their numbers began to dwindle.
+		`,
+	},
+	'61bd1dbc918f12c17b9c6476': {
+		img: '/assets/images/kedouin-clan.png',
+		description: `The Kedōin Clan (祁答院一族, Kedōin Ichizoku) is a small clan led by Agari Kaisen.
+			The clan was first introduced when Kiba Inuzuka, Hinata Hyūga and Naruto Uzumaki go on a treasure hunt mission. Agari's servants copied Naruto, Kiba, and Hinata's appearances and left them trapped in the cave while they left to go "mess up their village." The one posing as Naruto gave the real one a hard time and Kiba managed to take down Agari and the Kedōin clan members impersonating him and Hinata. Naruto finally defeated his impostor using the Shadow Clone Technique. Moments later in Tsunade's office, Tsunade revealed that she hired Agari to test the genin's teamwork and to see if they should be sent back to the Academy or not. Naruto, Kiba, and Hinata passed and collected the treasure chest, which contains the bill for Tsunade.
+		`,
+	},
+	'61bd1dbc918f12c17b9c6477': {
+		img: '/assets/images/kohaku-clan.png',
+		description: `The Kohaku Clan (Kohaku Ichizoku) was a shinobi clan that had a village near the border of the Land of Fire to that of Amegakure. This village was one of many villages that was attacked by Furido's 4-Man Team, and all villagers subsequently massacred.
+			The deceased members of this clan were later revived as zombies by their killers and used in Furido's attack on Konohagakure.
+			Despite having their own village, the clan was also seen to be affiliated with Konoha, since one of their revived members was seen wearing a Konoha flak jacket. The members had the symbol of black futatsudomoe (double tomoe shaped like yin and yang) tattooed on their upper arm with two lines going around it.
+		`,
+	},
+	'61bd1dbc918f12c17b9c6478': {
+		img: '/assets/images/kurama-clan.png',
+		description: `The Kurama Clan (鞍馬一族, Kurama Ichizoku) is a clan of extremely skilled genjutsu users, exclusive to the anime. This talent in genjutsu is due to the kekkei genkai that the clan possesses. However, once every few generations, a member of the clan will be born with such enormous skill in genjutsu that their illusions causes the brain to make anything that happens to the victim within the genjutsu physically real, allowing the clan member to potentially kill their opponents with genjutsu.
+			Such frightening power is not without its drawbacks, however. The user can rarely control the full extent of their abilities, and as such their subconscious will often regulate that power, leading to the creation of a second personality in control of that power. This personality then overwhelms the original, turning the individual into a monster that is a danger to everyone that surrounds them. They were originally one of the more prominent clans in Konohagakure, but the number of ninja in the clan has severely dwindled.
+		`,
+	},
+	'61bd1dbc918f12c17b9c6479': {
+		img: '/assets/images/nara-clan.png',
+		description: `The Nara Clan (奈良一族, Nara Ichizoku) or Nara Family (奈良家, Nara-ke) is one of the many clans of Konohagakure. They are known for tending deer and their ability to manipulate shadows through the use of Yin Release. Two of its members, Shikaku and his son Shikamaru, are well known for their high-level of intelligence, and both have served as advisors to the Hokage. After Shikaku's death, Shikamaru became the head of the clan.
+			The clan has kept a book with various medicine preparations and effects for generations, implying that they are skilled in the area of medicine and healing arts. Among the medicine described in this book are the secret pills used by the Akimichi clan. The Nara also have a special bond with the deer of the Nara Clan Forest, and only members of the clan are allowed inside without the deer attacking unless an outsider is given permission.
+		`,
+	},
+	'61bd1dbc918f12c17b9c647a': {
+		img: '/assets/images/sakon-and-ukon-clan.png',
+		description: `Sakon (左近, Sakon) and his older brother Ukon (右近, Ukon) were twin brothers from Otogakure and the apparent leader(s) of the Sound Four.
+			In the anime, the Sound Four were originally prisoners of Orochimaru, forced to fight other prisoners to the death in battle royales. By surviving their respective matches, they proved themselves to be the strongest of Orochimaru's prisoners and as such were made his bodyguards. The Sound Four were at some point forced to accept Kimimaro as their leader, who easily defeated them all in battle and, because of his Shikotsumyaku abilities, was slated to be Orochimaru's next host body.
+		`,
+	},
+	'61bd1dbc918f12c17b9c647b': {
+		img: '/assets/images/sarutobi-clan.png',
+		description: `The Sarutobi Clan (猿飛一族, Sarutobi Ichizoku) is an influential clan from Konohagakure. It has produced several renowned members who have held positions of high authority, including the Third Hokage; whose son, Asuma, held membership in the Twelve Guardian Ninja, while his other child served directly under the Hokage along with their spouse as Anbu.[1] All known members have also been devout observers of the Will of Fire.
+			After Konohagakure's formation following the Warring States Period, the Sarutobi were amongst the first set of clans to ally themselves with the Senju and Uchiha-founded village, alongside the Shimura clan.
+		`,
+	},
+	'61bd1dbc918f12c17b9c647c': {
+		img: '/assets/images/senju-clan.png',
+		description: `The Senju Clan (千手一族, Senju Ichizoku) was one of the clans responsible for founding the first shinobi village: Konohagakure — the other being their rival Uchiha clan. Although the clan itself no longer appears to exist as a group, the Senju continue to influence Konoha's politics through their philosophy of the Will of Fire and the corresponding interpretation of the role of the Hokage position. As such, three Senju have achieved the position of Hokage.
+			As descendants of Asura Ōtsutsuki, the Senju inherited his powerful life force and chakra, and through Hashirama Senju eventually manifested as Wood Release. It was thought that the Senju's strength was based in love. Compared to other shinobi clans, the Senju didn't specialise in just one area, instead mastering ninjutsu, taijutsu, and genjutsu to give themselves a wide variety of skills to combat their enemies.
+		`,
+	},
+	'61bd1dbc918f12c17b9c647d': {
+		img: '/assets/images/shiin-clan.png',
+		description: `The Shiin Clan was a small shinobi clan from the Land of the Rice Fields, where Orochimaru established the Land of Sound. Despite their size, they were famous in the country due to their high ideals and determination. After Otogakure was founded, all the skilled members joined Otogakure, and when the country's daimyō stopped hiring them, their leader set his eyes on a goldmine near the country's border, as a way to support his clan. He and the clan members were met with resistance at the village, and thus, they attacked and killed a majority of them. Menma panicked and tried to escape, but ended up falling off a cliff and losing his memory. He was later saved by Naruto.
+			Later, during their final attack on the village, the members were all defeated.
+			The clan has a hiden technique that allows them to erase another individual's memories.
+		`,
+	},
+	'61bd1dbc918f12c17b9c647e': {
+		img: '/assets/images/uchiha-clan.png',
+		description: `The Uchiha Clan (うちは一族, Uchiha Ichizoku) is one of the four noble clans of Konohagakure, reputed to be the village's strongest because of their Sharingan and natural battle prowess. After helping found Konoha decades ago, the Uchiha grew increasingly isolated from the village's affairs, culminating in most of their deaths during the Uchiha Clan Downfall. Few Uchiha now survive into the present day.
+			The Uchiha are descendants of Indra Ōtsutsuki, the elder son of Hagoromo Ōtsutsuki. Through Indra, the Uchiha inherited Hagoromo's "eyes", granting them powerful spiritual energy and chakra. As part of the so-called "Curse of Hatred" first displayed by Indra, Uchiha are subject to powerful emotions: these emotions typically start as love for a friend or family member that then become overpowering hatred when the object of their love is lost.
+		`,
+	},
+	'61bd1dbc918f12c17b9c647f': {
+		img: '/assets/images/uzumaki-clan.png',
+		description: `The Uzumaki Clan (うずまき一族, Uzumaki Ichizoku) was formerly a prominent clan in Uzushiogakure. Its members are renown for having bright, red hair, possessing incredibly strong life-forces and chakra, and their sealing prowess. Despite the fall of Uzushiogakure and decline of the clan, it never quite went extinct as most of its known members have since resided in Konohagakure the clan itself was recognised as active against 15 years after the Fourth Shinobi World War.
+			The Uzumaki, being descendants of Asura Ōtsutsuki, also shared distant blood relation with the Senju clan.[2] Through the years, the Uzumaki and Senju kept close ties, with members at times marrying between clans, as with Hashirama Senju and Mito Uzumaki.
+		`,
+	},
+	'61bd1dbc918f12c17b9c6480': {
+		img: '/assets/images/yamanaka-clan.png',
+		description: `The Yamanaka Clan (山中一族, Yamanaka Ichizoku) is a family of shinobi found in Konohagakure. They specialise in mind related techniques, and they own and run a flower shop in the village. They traditionally lead the Konoha Barrier Team.
+			The members of this clan specialise in mind-related techniques which makes them experts at intelligence gathering, espionage and interrogation amongst other things. They have also displayed sensory abilities and their techniques include transferring their consciousness, reading minds and communicating telepathically. The flower symbol of this clan is the bush clover which symbolises a "positive love".
+		`,
+	},
+	'61bd1dbc918f12c17b9c6481': {
+		img: '/assets/images/yotsuki-clan.png',
+		description: `The Yotsuki Clan (夜月一族, Yotsuki Ichizoku) is a clan from Kumogakure. During the First Raikage's reign they were given land in the Valley of Clouds and Lightning after swearing their loyalty to the village.
+			In Taka's attempt to capture Killer B, they confront Kiyoi Yotsuki. When he proved unwilling to sell out his allies, Sasuke used genjutsu to make him reveal Killer B's location and identifying features. According to Kiyoi, the Yotsuki will never willingly sell out their comrades.
+		`,
+	},
+	'61bd1dbc918f12c17b9c6482': {
+		img: '/assets/images/yuki-clan.png',
+		description: `The Yuki Clan (雪一族, Yuki Ichizoku) lived in the Land of Water. Some time ago, the country was in the midst of a civil war. In this war, different sides battled each other using ninja in combat, some of whom possessed kekkei genkai. After the war ended, memories of the gruesome battles still lingered in the minds of the people, causing wide-spread persecutions against those with the kekkei genkai. Because of this, the Yuki clan, whose members carried the Ice Release kekkei genkai went into hiding for their own safety.`,
+	},
+};
